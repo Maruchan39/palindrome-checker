b6a66f54095e0ead50e03894dc5e3bf53e85ed8a
Maruchan39 <66354349+Maruchan39@users.noreply.github.com>
2020-10-29 22:33:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var input = document.getElementById("text");
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("button").click();
@@ -7,31 +7,30 @@ input.addEventListener("keyup", function(event) {
 });
 
 document.getElementById("text").value;
-    input.addEventListener("keyup", function(event) {
-      if (event.keyCode === 13) {
-          document.getElementById("text").click();
-      }
-  });
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    document.getElementById("text").click();
+  }
+});
 
 function palindrome() {
-    let word = document.getElementById("text").value;
-    function reverseString(str) {
-      return str.split("").reverse().join("");
-  }
-  
-    str = word.toLowerCase().replace(/[^a-z0-9]/g,'');
-    let reversedStr = reverseString(str);
-    if(str === reversedStr) {
-        document.getElementById("output").innerHTML = word + " is a palindrome!";
-    }
-    else document.getElementById("output").innerHTML = word + " is not a palindrome";
+  let word = document.getElementById("text").value;
+  function reverseString(str) {
+    return str.split("").reverse().join("");
   }
 
-  function ClearFields() {
+  str = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let reversedStr = reverseString(str);
+  if (str === reversedStr) {
+    document.getElementById("output").innerHTML = word + " is a palindrome!";
+  }
+  else document.getElementById("output").innerHTML = word + " is not a palindrome";
+}
 
-    document.getElementById("text").value = "";
+function ClearFields() {
+
+  document.getElementById("text").value = "";
 }
 
 
-  
-  
+
